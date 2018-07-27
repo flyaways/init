505c428558c74bf58bab1a2dd08b46413b89392d
flyaways <meagleglass@gmail.com>
2018-07-27 10:06:37 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -4,6 +4,7 @@ git config --global color.status auto
 git config --global color.diff auto
 git config --global color.branch auto
 git config --global color.interactive auto
+git config --global color.ui true
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
