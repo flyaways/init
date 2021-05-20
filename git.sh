683ad609f6b4f8279835099add5a5e79c739fb32
flyaways <9859316+flyaways@users.noreply.github.com>
2021-05-20 11:37:59 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -10,3 +10,6 @@ git config --global pull.rebase true
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 git config --global alias.lgh "log --graph --decorate --oneline --pretty=format:'%Cred%h - %C(bold white) %s %Creset %C(yellow)%d  %C(cyan) <%cd> %Creset %Cgreen(%cn)' --abbrev-commit --date=format:'%Y-%m-%d %H:%M:%S'"
+
+
+git config --global url."https://github.com.cnpmjs.org".insteadOf "https://github.com"
